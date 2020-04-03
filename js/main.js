bd3d985a81664f5fa7ad677504ae1bd6602b16db
Rafael Morán <rm.veiga26@gmail.com>
2020-04-03 15:48:12 -0300

    Aula 2: Array e dicionário

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
+/* AULA 1
 var nome = "Rafael"
 alert("Bem vindo " + nome);
-
 console.log(nome);
 console.log(nome.toLocaleUpperCase())
+*/
+
+/* AULA 2
+// ARRAY
+var lista = ["vinho", "cerveja", "tequila"]
+//alert(lista[2])
+lista.push("whisky")
+//alert(lista)
+lista.pop("vinho")
+console.log(lista.reverse())
+console.log(lista.join(" - "))
+
+
+// DICIONÁRIO
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxo"}]
+console.log(frutas[1])
+alert(frutas.length)
+*/
