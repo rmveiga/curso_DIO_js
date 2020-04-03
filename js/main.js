c2324935be38eb41dd2f93d17bc9ad344a0e43d6
Rafael Morán <rm.veiga26@gmail.com>
2020-04-03 18:26:33 -0300

    Aula 3: Condicionais, laços de repetição e data

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-/* AULA 1
-var nome = "Rafael"
-alert("Bem vindo " + nome);
-console.log(nome);
-console.log(nome.toLocaleUpperCase())
+/* AULA 3
+var idade = prompt("Informe a sua idade")
+if (idade >= 18){
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+
+var cont = 0
+while (cont < 5){
+    console.log(cont)
+    cont++
+}
+
+for (cont = 1; cont <= 5; cont++){
+    console.log(cont)
+}
+
+var data_atual = new Date
+alert(data_atual.getDate() + "/" + (data_atual.getMonth() + 1) + "/" +data_atual.getFullYear())
 */
 
 /* AULA 2
@@ -20,4 +35,11 @@ console.log(lista.join(" - "))
 var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxo"}]
 console.log(frutas[1])
 alert(frutas.length)
+*/
+
+/* AULA 1
+var nome = "Rafael"
+alert("Bem vindo " + nome);
+console.log(nome);
+console.log(nome.toLocaleUpperCase())
 */
